83683a0ab66212ab4b5ee07e0ed62a5df6ad713c
devbylin <saulhstore@gmail.com>
2026-06-24 22:13:45 -0500

    crear funciones,modificar onload difinir variables y constantes

--- cazando.js
+++ cazando.js
@@ -1,17 +1,37 @@
+
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas .getContext("2d");
 
-function iniciar(){
+//constantes 
+const ALTO_GATO = 200;
+const ANCHO_GATO = 100;
+const ALTO_COMIDA = 80;
+const ANCHO_COMIDA = 80;
+
+
+//variables
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+
+
+function iniciarJuego(){
+    gatoX = (canvas.width - ANCHO_GATO) / 2;
+    gatoY = (canvas.height - ALTO_GATO) / 2;
+    comidaX = 0;
+    comidaY = canvas.height-80;
     graficarGato();
+    graficarComida();
 }
 
-function graficarGato(){
-    let anchoRect = 100;
-    let altoRect = 50;
-    
-    let x = (canvas.width - anchoRect) / 2;
-    let y = (canvas.height - altoRect) / 2;
-    
+function graficarGato() {
     ctx.fillStyle = "orange";
-    ctx.fillRect(x, y, anchoRect, altoRect);
+    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+}
+
+function graficarComida() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
 }
