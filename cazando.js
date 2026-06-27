7cd2fda95e8874027e435ee9871232fe2ff85a8d
devbylin <saulhstore@gmail.com>
2026-06-27 15:15:24 -0500

    mover en todas las direcciones

--- cazando.js
+++ cazando.js
@@ -8,15 +8,11 @@ const ANCHO_GATO = 50;
 const ALTO_COMIDA = 30;
 const ANCHO_COMIDA = 30;
 
-
-
 //variables
 let gatoX = 0;
 let gatoY = 0;
 let comidaX = 0;
 let comidaY = 0;
-
-
 
 function iniciarJuego(){
     gatoX = (canvas.width - ANCHO_GATO) / 2;
@@ -30,7 +26,6 @@ function graficarRectangulo(x, y, ancho, alto, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, ancho, alto);
 }
-
 function graficarGato() {
    graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "orange");
 }
@@ -39,10 +34,28 @@ function graficarComida() {
    graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "green");
 }
 function limpiarCanva(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+   ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 function moverIzquierda(){
     gatoX = gatoX - 10;
+    limpiarCanva();
+    graficarGato();
+    graficarComida();  
+}
+function moverDerecha(){
+    gatoX = gatoX + 10;
+    limpiarCanva();
+    graficarGato();
+    graficarComida();  
+}
+function moverArriba(){
+    gatoY = gatoY - 10;
+    limpiarCanva();
+    graficarGato();
+    graficarComida();  
+}
+function moverAbajo(){
+    gatoY = gatoY + 10;
     limpiarCanva();
     graficarGato();
     graficarComida();  
